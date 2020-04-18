8f735f096b463473172569c9bba22d04605b01e5
Dinis Cruz <dinis.cruz@owasp.org>
2020-04-18 20:10:11 +0100

    couple changes

--- tests/setup/test_setup.py
+++ tests/setup/test_setup.py
@@ -1,10 +1,13 @@
 from unittest import TestCase
-
+import os
 
 class test_setup(TestCase):
     def test_first_one(self):
-        print('------')
-        print('in unit test .... ')
+        print('------checking out secret ---- ')
+        print(f'PWD: {os.getenv("PWD")}')
+        print(f'PWD: {os.getenv("SUPER_SECRET")}')
+
+
 
     def test_second(self):
         print('here')
