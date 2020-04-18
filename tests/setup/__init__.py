dc0242f2c54d1f7f2773bea432300f10248bb89b
Dinis Cruz <dinis.cruz@owasp.org>
2020-04-18 20:29:26 +0100

    couple changes

--- tests/setup/__init__.py
+++ tests/setup/__init__.py
@@ -4,3 +4,10 @@ import sys
 
 sys.path.append('./modules/GW-Bot/modules/OSBot-AWS')
 sys.path.append('./modules/GW-Bot/modules/OSBot-Utils')
+
+# setup dependencies in github Actions
+
+sys.path.append('../OSBot-AWS')
+sys.path.append('../OSBot-Utils')
+
+
