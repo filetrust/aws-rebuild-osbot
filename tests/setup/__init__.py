b9a42158baf0cff944dc04562f90d4626b468973
Dinis Cruz <dinis.cruz@owasp.org>
2020-04-18 20:26:42 +0100

    couple changes

---
+++ tests/setup/__init__.py
@@ -0,0 +1,6 @@
+import sys
+
+# setup dependencies locally
+
+sys.path.append('./modules/GW-Bot/modules/OSBot-AWS')
+sys.path.append('./modules/GW-Bot/modules/OSBot-Utils')
--- tests/setup/test_setup.py
+++ tests/setup/test_setup.py
@@ -8,7 +8,7 @@ class test_setup(TestCase):
     def test_first_one(self):
         print('------checking out secret ---- ')
         print(f'PWD: {os.getenv("PWD")}')
-        print(f'PWD: {len(os.getenv("SUPER_SECRET"))}')
+        print(f'PWD: {len(os.getenv("SUPER_SECRET",""))}')
         print(f'PWD: {os.getenv("SUPER_SECRET")}')
 
 
