23f586755fdbcf7480a1b25038dfddfb0a3f1a65
Dinis Cruz <dinis.cruz@owasp.org>
2020-04-18 20:18:49 +0100

    couple changes

--- tests/setup/test_setup.py
+++ tests/setup/test_setup.py
@@ -1,16 +1,21 @@
 from unittest import TestCase
 import os
 
+from osbot_aws.apis.S3 import S3
+
+
 class test_setup(TestCase):
     def test_first_one(self):
         print('------checking out secret ---- ')
         print(f'PWD: {os.getenv("PWD")}')
+        print(f'PWD: {len(os.getenv("SUPER_SECRET"))}')
         print(f'PWD: {os.getenv("SUPER_SECRET")}')
 
 
 
     def test_second(self):
-        print('here')
+        s3 = S3()
+        print(s3.buckets())
 
     def test_third(self):
         pass
